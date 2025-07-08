be3f205d74632ff577bd0d515324d0fd567e3f91
shirishkurariya18 <harshkurariya2005@gmail.com>
2025-07-08 17:16:07 +0530

    cpp

---
+++ intersection_of_array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    unordered_set<int> set1(nums1.begin(), nums1.end());
+    unordered_set<int> result;
+
+    for (int num : nums2) {
+        if (set1.count(num)) {
+            result.insert(num);
+        }
+    }
+
+    return vector<int>(result.begin(), result.end());
+}
+int main() {
+    vector<int> nums1 = {1, 2, 2, 1};
+    vector<int> nums2 = {2, 2};
+
+    vector<int> result = intersection(nums1, nums2);
+
+    cout << "Intersection: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}

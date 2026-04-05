8478a069497c9651442ac1e0edc8a6fc011e1652
shirishkurariya18 <harshkurariya2005@gmail.com>
2026-04-05 16:19:57 +0530

    cpp

---
+++ rectangular_histogram.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int largestRectangleArea(vector<int>& heights) {
+    stack<int> st;
+    int maxArea = 0;
+    heights.push_back(0); 
+
+    for (int i = 0; i < heights.size(); i++) {
+        while (!st.empty() && heights[st.top()] > heights[i]) {
+            int h = heights[st.top()];
+            st.pop();
+            int width = st.empty() ? i : i - st.top() - 1;
+            maxArea = max(maxArea, h * width);
+        }
+        st.push(i);
+    }
+    return maxArea;
+}
+
+int main() {
+    vector<int> heights = {2, 1, 5, 6, 2, 3};
+    cout << largestRectangleArea(heights) << endl; // Output: 10
+    return 0;
+}

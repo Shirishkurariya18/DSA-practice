b31ed2235c058e3996147488238c0f814981894b
shirishkurariya18 <harshkurariya2005@gmail.com>
2026-05-04 16:11:28 +0530

    cpp

---
+++ add_string.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    string str1  = "harsh" , str2 = "kurariya", str3 = str1 + str2;
+    cout << str3 << endl;
+
+return 0;
+}
